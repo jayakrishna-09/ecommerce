e91b89520e0144bc81a8d2066bab92c2bb7bb56c
unknown <TAnne@its.jnj.com>
2025-09-17 11:08:26 +0530

    delting file

--- backend/routes/customerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// temporary test route
-router.get("/", (req, res) => {
-  res.send("Customer routes working...");
-});
-
-export default router;
--- backend/routes/vendorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// temporary test route
-router.get("/", (req, res) => {
-  res.send("Vendor routes working...");
-});
-
-export default router;
